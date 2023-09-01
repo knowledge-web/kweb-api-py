f1d93ebf3f8d23f045bd20efc4c22e1d9d575050
gorbiz <gorbiz@gmail.com>
2023-09-01 01:55:35 +0000

    More wikilink fix, untested

--- 50-wikilink-extractor.py
+++ 50-wikilink-extractor.py
@@ -1,38 +1,35 @@
 import os
 import re
-import json
 import sqlite3
+import zipfile
 
-# TODO modify this to add nodes.wikilink to kweb.db instead (if db not existing; create?)
-
-# TODO default to read thoughts from ./Brain.db thoughts table instead of JSON file. and content.zip instead of B02 folder 
 # Define paths
-db_folder_path = 'path/to/db'
-b02_folder_path = 'path/to/B02'
+db_folder_path = '.'
+content_zip_path = './content.zip'
 
-# Load the thoughts data from filtered_thoughts.json
-with open(os.path.join(db_folder_path, 'filtered_thoughts.json'), 'r') as f:
-    thoughts_data = json.load(f)
+# Fetch thoughts from Brain.db
+conn_brain = sqlite3.connect(os.path.join(db_folder_path, 'Brain.db'))
+c_brain = conn_brain.cursor()
+c_brain.execute('SELECT * FROM thoughts')
+thoughts_data = c_brain.fetchall()
+conn_brain.close()
 
 # Regular expressions to find Markdown links
 markdown_link_regex = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
 name_wikipedia_link_regex = re.compile(r'name:.*?\[([^\]]+)\]\((https://en\.wikipedia\.org[^)]+)\)', re.IGNORECASE)
 
 # Initialize a dictionary to map thought Ids to Wikipedia links or NULL
-id_to_wikilink_map = {thought['Id']: None for thought in thoughts_data}
+id_to_wikilink_map = {str(thought[0]): None for thought in thoughts_data}
 
-# Loop through the .md files in B02 to update the Wikipedia links or keep NULL
-for root, dirs, files in os.walk(b02_folder_path):
-    for file in files:
+# Loop through the .md files in content.zip to update the Wikipedia links or keep NULL
+with zipfile.ZipFile(content_zip_path, 'r') as zip_ref:
+    for file in zip_ref.namelist():
         if file.endswith('.md'):
-            file_path = os.path.join(root, file)
-            folder_name = os.path.basename(os.path.dirname(file_path))
+            folder_name = os.path.basename(os.path.dirname(file))
             
-            # Read the .md file content
-            with open(file_path, 'r', encoding='utf-8') as f:
-                content = f.read()
-            
-            # Try to find a Wikipedia link matching any of the patterns
+            with zip_ref.open(file) as f:
+                content = f.read().decode('utf-8')
+                
             wikilink = None
             for link_text, link_url in markdown_link_regex.findall(content):
                 if 'wikipedia.org' in link_url:
@@ -42,78 +39,17 @@ for root, dirs, files in os.walk(b02_folder_path):
                 wikilink = link_url
                 break
             
-            # Update the mapping with the found link or keep NULL
             id_to_wikilink_map[folder_name] = wikilink
 
-# Create SQLite3 database and table
-db_path = 'kweb.db'
-conn = sqlite3.connect(db_path)
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS wikipedia_links (id TEXT PRIMARY KEY, wikiimport os
-import re
-import json
-import sqlite3
+# Connect to kweb.db, create if not existing
+conn_kweb = sqlite3.connect('kweb.db')
+c_kweb = conn_kweb.cursor()
+c_kweb.execute('''CREATE TABLE IF NOT EXISTS nodes (id TEXT PRIMARY KEY, wikilink TEXT)''')
 
-# TODO modify this to add nodes.wikilink to kweb.db instead (if db not existing; create?)
-
-# Define paths
-db_folder_path = 'path/to/db'
-b02_folder_path = 'path/to/B02'
-
-# Load the thoughts data from filtered_thoughts.json
-with open(os.path.join(db_folder_path, 'filtered_thoughts.json'), 'r') as f:
-    thoughts_data = json.load(f)
-
-# Regular expressions to find Markdown links
-markdown_link_regex = re.compile(r'\[([^\]]+)\]\(([^)]+)\)')
-name_wikipedia_link_regex = re.compile(r'name:.*?\[([^\]]+)\]\((https://en\.wikipedia\.org[^)]+)\)', re.IGNORECASE)
-
-# Initialize a dictionary to map thought Ids to Wikipedia links or NULL
-id_to_wikilink_map = {thought['Id']: None for thought in thoughts_data}
-
-# Loop through the .md files in B02 to update the Wikipedia links or keep NULL
-for root, dirs, files in os.walk(b02_folder_path):
-    for file in files:
-        if file.endswith('.md'):
-            file_path = os.path.join(root, file)
-            folder_name = os.path.basename(os.path.dirname(file_path))
-            
-            # Read the .md file content
-            with open(file_path, 'r', encoding='utf-8') as f:
-                content = f.read()
-            
-            # Try to find a Wikipedia link matching any of the patterns
-            wikilink = None
-            for link_text, link_url in markdown_link_regex.findall(content):
-                if 'wikipedia.org' in link_url:
-                    wikilink = link_url
-                    break
-            for link_text, link_url in name_wikipedia_link_regex.findall(content):
-                wikilink = link_url
-                break
-            
-            # Update the mapping with the found link or keep NULL
-            id_to_wikilink_map[folder_name] = wikilink
-
-# Create SQLite3 database and table
-db_path = 'kweb.db'
-conn = sqlite3.connect(db_path)
-c = conn.cursor()
-c.execute('''CREATE TABLE IF NOT EXISTS wikipedia_links (id TEXT PRIMARY KEY, wikilink TEXT)''')
-
-# Insert the mapped data into the table
+# Insert/Update the mapped data into the table
 for thought_id, wikilink in id_to_wikilink_map.items():
-    c.execute("INSERT INTO wikipedia_links (id, wikilink) VALUES (?, ?)", (thought_id, wikilink))
+    c_kweb.execute("INSERT OR REPLACE INTO nodes (id, wikilink) VALUES (?, ?)", (thought_id, wikilink))
 
 # Commit and close
-conn.commit()
-conn.close()
-link TEXT)''')
-
-# Insert the mapped data into the table
-for thought_id, wikilink in id_to_wikilink_map.items():
-    c.execute("INSERT INTO wikipedia_links (id, wikilink) VALUES (?, ?)", (thought_id, wikilink))
-
-# Commit and close
-conn.commit()
-conn.close()
+conn_kweb.commit()
+conn_kweb.close()

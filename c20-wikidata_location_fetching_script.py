8113737c97c524d75bd876f549a3bf9e087797d5
gorbiz <gorbiz@gmail.com>
2023-09-10 15:57:51 +0000

    Locations

--- c20-wikidata_location_fetching_script.py
+++ c20-wikidata_location_fetching_script.py
@@ -3,36 +3,46 @@ import json
 import time
 import requests
 
-# Function to fetch location details from Wikidata
-def fetch_location_details(location_id):
+# Function to fetch entity details from Wikidata
+def fetch_entity_details(entity_id):
     try:
-        url = f"https://www.wikidata.org/wiki/Special:EntityData/{location_id}.json"
+        url = f"https://www.wikidata.org/wiki/Special:EntityData/{entity_id}.json"
         response = requests.get(url)
         data = response.json()
-        
-        # Logic to extract place name
-        entity = data.get("entities", {}).get(location_id, {})
-        labels = entity.get("labels", {}).get("en", {}).get("value", "")
-
-        # Logic to extract country (P17 is the property for 'country')
-        claims = entity.get("claims", {})
-        country_claim = claims.get("P17", [{}])[0].get("mainsnak", {}).get("datavalue", {}).get("value", {}).get("id", "")
-        country = "Unknown" if not country_claim else country_claim  # Replace this with actual country name fetched from another API call if necessary
-        
-        # Logic to extract coordinates (P625 is the property for 'coordinate location')
-        coordinates_claim = claims.get("P625", [{}])[0].get("mainsnak", {}).get("datavalue", {}).get("value", {})
-        latitude = coordinates_claim.get("latitude", 0.0)
-        longitude = coordinates_claim.get("longitude", 0.0)
-        coordinates = [latitude, longitude]
-
-        return {
-            'name': labels,
-            'country': country,  # This will be the country's Wikidata ID, not its name
-            'coordinates': coordinates
-        }
+        entity = data.get("entities", {}).get(entity_id, {})
+        return entity
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
+
+# Function to fetch location details from Wikidata
+def fetch_location_details(location_id):
+    entity = fetch_entity_details(location_id)
+    if not entity:
+        return None
+    
+    # Logic to extract place name
+    labels = entity.get("labels", {}).get("en", {}).get("value", "")
+
+    # Logic to extract country (P17 is the property for 'country')
+    claims = entity.get("claims", {})
+    country_claim = claims.get("P17", [{}])[0].get("mainsnak", {}).get("datavalue", {}).get("value", {}).get("id", "")
+    
+    # Fetch the country name in English
+    country_entity = fetch_entity_details(country_claim)
+    country = country_entity.get("labels", {}).get("en", {}).get("value", "") if country_entity else "Unknown"
+    
+    # Logic to extract coordinates (P625 is the property for 'coordinate location')
+    coordinates_claim = claims.get("P625", [{}])[0].get("mainsnak", {}).get("datavalue", {}).get("value", {})
+    latitude = coordinates_claim.get("latitude", 0.0)
+    longitude = coordinates_claim.get("longitude", 0.0)
+    coordinates = [latitude, longitude]
+
+    return {
+        'name': labels,
+        'country': country,
+        'coordinates': coordinates
+    }
 
 # Read the previously generated JSON to continue from where it left off
 try:
